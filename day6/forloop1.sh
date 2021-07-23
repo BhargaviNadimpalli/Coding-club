a446027bae09a4cd59db8bf8a2a02cb64dbbe9dc
BhargaviNadimpalli <87687249+BhargaviNadimpalli@users.noreply.github.com>
2021-07-23 23:27:56 +0530

    Delete day6 directory

--- day6/forloop1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-for(( counter=10; counter>0; counter=$((counter-2)) ))
-do
-    echo "$counter"
-done
--- day6/printfunction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-function print_msg()
-{
-     echo "hello $1"
-}
-read -p "enter parameter of function :" param
-print_msg $param
